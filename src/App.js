b21b17d24f2ed509c7f838cae5c5edd8e9c922e6
HenriqueRulez <mirandaferreirahenrique@hotmail.com>
2022-11-17 17:01:42 +0000

    subjects: componentDidMount, fetchAPI, Prommise.all, minor grid and css aspects

--- src/App.js
+++ src/App.js
@@ -7,18 +7,41 @@ class App extends Component {
   }
 
   componentDidMount(){
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+
+    const [posts, photos] = await Promise.all([postsResponse, photoResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    //Zip menor array (posts) com maior array (photos)
+    const postsAndPhotos = postsJson.map((posts,index) => {
+      return { ...posts, cover: photosJson[index].url}
+    })
+
+    this.setState({posts: postsAndPhotos})
   }
 
   render(){
     const { posts } = this.state
     return(
-      <div className="App">
+      <div className="container">
+        <article className="posts">
         {posts.map(post => (
-          <div key={post.id}>
+          <div className="post">
+            <img src={post.cover} alt={post.title} />
+            <section key={post.id} className="post-content">
             <h1>{post.title}</h1>
             <p>{post.body}</p>
+          </section>
           </div>
         ))}
+      </article>
       </div>
     )
   }
